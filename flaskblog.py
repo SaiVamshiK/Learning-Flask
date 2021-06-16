37c659ab1aa5d7cd6d54d8a19bc8ac1e2a824b0f
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-17 00:31:03 +0530

    passing dummy posts to templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,25 @@
 from flask import Flask,render_template
 app=Flask(__name__)
 
+posts=[
+    {
+        'author':'Sai Vamshi',
+        'title':'Blog Post 1',
+        'content':'First blog post',
+        'date_posted':'April 18 2021'
+    },
+    {
+        'author':'KL Rahul',
+        'title':'Blog Post 2',
+        'content':'Second blog post',
+        'date_posted':'April 20 2021'
+    },
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=posts)
 
 @app.route("/about")
 def about():
