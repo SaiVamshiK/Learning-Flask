90046593ec367cc1468cfe85f4e83a3c10040067
SaiVamshiK <saivamshi.k.24@gmail.com>
2021-06-17 10:34:35 +0530

    styled forms and added allowed methods for a route

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,url_for
+from forms import RegistrationForm,LoginForm
 app=Flask(__name__)
 
 app.config['SECRET_KEY']='e5aeeed185224376330bc6b0a1b0de38'
@@ -26,6 +27,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/login")
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login',form=form)
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form=RegistrationForm()
+    return render_template('register.html',title='Register',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
